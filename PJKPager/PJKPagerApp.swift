47d9436186de7debe0c23bcce5ddd44fdbf8c216
Kashyap Chaturvedula <kashyapc@seas.upenn.edu>
2022-11-08 00:11:10 -0800

    Initial Commit

---
+++ PJKPager/PJKPagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PJKPagerApp.swift
+//  PJKPager
+//
+//  Created by Kashyap Chaturvedula on 11/8/22.
+//
+
+import SwiftUI
+
+@main
+struct PJKPagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
